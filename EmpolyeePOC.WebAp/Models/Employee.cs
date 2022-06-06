db370f71091618575b330844ef218a32021b1881
Rahul <rahulkhapne44@gmail.com>
2022-06-06 19:19:12 +0530

    Project updated

---
+++ EmpolyeePOC.WebAp/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace EmpolyeePOC.WebAp.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+
+        public string EmpContact { get; set; }
+        public string EmpEmail { get; set; }
+        public string EmpAddress { get; set; }
+
+    }
+}
---
+++ EmpolyeePOC.WebAp/Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EmpolyeePOC.WebAp.Models
+{
+    public class EmployeeContext:DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+        //dbset
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
